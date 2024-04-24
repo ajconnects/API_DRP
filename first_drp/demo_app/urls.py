42545b07703c20604a7905362eef95bf23b4ac10
Aj connect <ajiriiviero2021@gmail.com>
2024-04-24 21:34:40 +0200

    new

--- first_drp/demo_app/urls.py
+++ first_drp/demo_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("home/", views.Home_page),
     path("demo-version/", views.demo_version),
+    path('custom-version/', views.DemoView.as_view()),
+    path('another-custom-version/', views.AnotherView.as_view()),
 ]
